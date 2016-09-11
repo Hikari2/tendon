fb54016c1e97267a1c22e897fa6855344da137cd
Hikari2 <youguruto2@gmail.com>
2016-09-11 16:25:26 +0200

    Initial commit

---
+++ android/app/src/main/java/com/tendon/MainActivity.java
@@ -0,0 +1,11 @@
+package com.tendon;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    @Override
+    protected String getMainComponentName() {
+        return "tendon";
+    }
+}
